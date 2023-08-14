7d795673a74beec167974312663d74ead222e128
Mohamed Aziz Koumenji <113291493+mohamedazizkoumenji@users.noreply.github.com>
2023-08-14 17:50:50 +0100

    Finished exercise 6.9

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -2,16 +2,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { vote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  let anecdotes = useSelector((state) => state.anecdotes);
+  const filter = useSelector((state) => state.filter);
   const dispatch = useDispatch();
 
   function compareNumbers(a, b) {
     return b.votes - a.votes;
   }
+
+  anecdotes = anecdotes.sort(compareNumbers);
+  const regex = new RegExp(filter, "i");
+  anecdotes = anecdotes.filter((object) => regex.test(object.content));
   return (
     <>
       <h2>Anecdotes</h2>
-      {anecdotes.sort(compareNumbers).map((anecdote) => (
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
