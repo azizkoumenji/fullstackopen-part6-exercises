cf8cc3672e7fc8ca6e97b516917c5767fad3f6a9
Mohamed Aziz Koumenji <113291493+mohamedazizkoumenji@users.noreply.github.com>
2023-08-18 12:00:13 +0100

    Finished exercise 6.14

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,9 +1,18 @@
+import { useDispatch } from "react-redux";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import { useEffect } from "react";
+import anecdoteService from "./services/anecdotes";
+import { setAnecs } from "./reducers/anecdoteReducer";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdoteService.getAll().then((notes) => dispatch(setAnecs(notes)));
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div>
       <Notification />
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
 const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
@@ -19,11 +10,9 @@ const asObject = (anecdote) => {
   };
 };
 
-const initialState = anecdotesAtStart.map(asObject);
-
 const anecdoteSlice = createSlice({
   name: "anecdotes",
-  initialState: initialState,
+  initialState: [],
   reducers: {
     newAnec(state, action) {
       return [...state, asObject(action.payload)];
@@ -37,8 +26,11 @@ const anecdoteSlice = createSlice({
       };
       return state.map((anec) => (anec.id !== id ? anec : changedAnec));
     },
+    setAnecs(state, action) {
+      return action.payload;
+    },
   },
 });
 
-export const { newAnec, vote } = anecdoteSlice.actions;
+export const { newAnec, vote, setAnecs } = anecdoteSlice.actions;
 export default anecdoteSlice.reducer;
