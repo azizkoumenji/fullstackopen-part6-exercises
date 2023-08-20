50a433470caf6599780f522c5db481fbb6b2f67a
Mohamed Aziz Koumenji <113291493+mohamedazizkoumenji@users.noreply.github.com>
2023-08-20 18:41:32 +0100

    Finished exercise 6.21

--- query-anecdotes/src/components/AnecdoteForm.js
+++ query-anecdotes/src/components/AnecdoteForm.js
@@ -1,21 +1,33 @@
-const AnecdoteForm = () => {
+import { createAnecdote } from "../requests";
+import { useMutation, useQueryClient } from "react-query";
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+
+  const newAnecMutation = useMutation(createAnecdote, {
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData("anecdotes");
+      console.log(anecdotes);
+      queryClient.setQueryData("anecdotes", anecdotes.concat(newAnecdote));
+    },
+  });
+
+  const onCreate = async (event) => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    newAnecMutation.mutate({ content, votes: 0 });
+  };
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
